6ecb104a1fd14a289164ae90097bff161005af7a
Van Nguyen <tovannguuyen712@gmail.com>
2021-07-05 15:57:04 +0700

    update login and binding signup

--- NetflixApplication/ViewModels/SignUp1ViewModel.cs
+++ NetflixApplication/ViewModels/SignUp1ViewModel.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using NetflixApplication.Models;
 
 namespace NetflixApplication.ViewModels
 {
     public class SignUp1ViewModel : BaseViewModel
     {
-        SignUpViewModel vm;
+        
+        public bool Exist { get; set; }
         public ICommand SignUpCommand { get; set; }
         public ICommand SignUp2Command { get; set; }
 
@@ -25,7 +29,25 @@ namespace NetflixApplication.ViewModels
         }
         public SignUp1ViewModel()
         {
-            
+            Exist = false;
+            SignUpCommand = new RelayCommand<Window>((p) => { return true; }, (p) => { CheckEmail(p); });
+        }
+        void CheckEmail(Window p)
+        {
+            if (p == null)
+                return;
+            var email = DataProvider.Ins.DB.Accounts.Where(x => x.Account_Email == Email).Count();
+            if(email < 1)
+            {
+                Exist = true;
+                MessageBox.Show("Chua ton tai email");
+            }
+            else
+            {
+                Exist = false;
+                MessageBox.Show("Email da ton tai");
+            }
         }
     }
+
 }
