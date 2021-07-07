0c1fc0f65be72b2f9de166b295b81430684e40d3
Van Nguyen <tovannguuyen712@gmail.com>
2021-07-07 16:28:43 +0700

    complete signup1view

--- NetflixApplication/ViewModels/SignUp1ViewModel.cs
+++ NetflixApplication/ViewModels/SignUp1ViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using NetflixApplication.Models;
+using NetflixApplication.Views;
 
 namespace NetflixApplication.ViewModels
 {
@@ -17,30 +22,24 @@ namespace NetflixApplication.ViewModels
         public ICommand SignUpCommand { get; set; }
         public ICommand SignUp2Command { get; set; }
 
-        private string _email;
-
-        public string Email
-        {
-            get { return _email; }
-            set
-            {
-                OnPropertyChanged(ref _email, value);
-            }
-        }
+        private string _Account_Email;
+        public string Account_Email { get => _Account_Email; set { _Account_Email = value; OnPropertyChanged(); } }
         public SignUp1ViewModel()
         {
             Exist = false;
-            SignUpCommand = new RelayCommand<Window>((p) => { return true; }, (p) => { CheckEmail(p); });
+            Account_Email = "";
+            SignUpCommand = new RelayCommand<Page>((p) => { return true; }, (p) => { CheckEmail(p); });
         }
-        void CheckEmail(Window p)
+        void CheckEmail(Page p)
         {
             if (p == null)
-                return;
-            var email = DataProvider.Ins.DB.Accounts.Where(x => x.Account_Email == Email).Count();
-            if(email < 1)
+                return;            
+            var email = DataProvider.Ins.DB.Accounts.Where(x => x.Account_Email == Account_Email).Count();
+            if (email == 0)
             {
                 Exist = true;
-                MessageBox.Show("Chua ton tai email");
+                MessageBox.Show("Email chua ton tai");
+                p.NavigationService.Navigate(new SignUp2View());
             }
             else
             {
